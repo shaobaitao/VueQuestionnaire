702541bdf95e8e95c6a16d1d5133984b55e9c601
shaobaitao <noob@shaobaitao.cn>
2021-01-20 22:54:58 +0800

    Vue问卷1.1

--- src/views/Analysis.vue
+++ src/views/Analysis.vue
@@ -10,6 +10,7 @@
             <el-table
                 :data="item.options"
                 stripe
+                :summary-method="getSummaries"
                 show-summary="show-summary"
                 style="width: 100%">
                 <el-table-column
@@ -148,6 +149,32 @@ export default {
                     return "【填空题】";
             }
         },
+        getSummaries(param) {
+            const { columns, data } = param;
+
+            const sums = [];
+            columns.forEach((column, index) => {
+                if (index === 2) {
+                    sums[index] = '';
+                    return;
+                }
+                const values = data.map(item => Number(item[column.property]));
+                if (!values.every(value => isNaN(value))) {
+                    sums[index] = values.reduce((prev, curr) => {
+                        const value = Number(curr);
+                        if (!isNaN(value)) {
+                            return prev + curr;
+                        } else {
+                            return prev;
+                        }
+                    }, 0);
+                    // sums[index] += ' 元';
+                } else {
+                    sums[index] = '总计';
+                }
+            });
+            return sums;
+        }
     },
 
     mounted() {
